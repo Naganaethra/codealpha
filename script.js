827fc77e9201f2e05619ef9aa9dbeab570d84c4b
Naganaethra <727723eucs135@skcet.ac.in>
2025-01-15 18:05:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let menu=documrnt.querySelector('#menu-icon');
+let navbar=documrnt.querySelector('.navbar');
+
+menu.onclick=()=>{
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+window.onscroll=()=>{
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+const typed = new Typed('.multiple-text', {
+    strings: ['Frontend Developer', 'Backend Developer', 'Blockchain Developer', 'Web Designer', 'YouTuber'],
+    typeSpeed: 80,
+    backSpeed:80,
+    backDelay:1200,
+    loop:true,
+  });
